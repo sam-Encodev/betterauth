b2c42801f8b05b0c085a676c05193fad7e4af64d
Sam <samuel@encodevlabs.com>
2025-11-15 10:12:36 +0000

    port

--- api/app.ts
+++ api/app.ts
@@ -1,7 +1,7 @@
 import { app } from "./index";
 import { config } from "./config";
 
-const port = config.server.port || 4000;
+const port = Number(config.server.port) || 4000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
